74c4e902d1210acd8d3d03da88c0c608891a16da
Timofey <tsprokhorov@outlook.com>
2024-10-22 14:55:01 +0300

    commit #2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
+app1 = FastAPI()
+@app1.get("/")
 def function():
     return "Hello, world"
+@app1.get("/sum_numbers", summary = "Вывод суммы двух чисел")
+def numbers_sum(a:int,b:int) -> int:
+    """ что вы хотели здесь увидеть???"""
+    return a+b
 
