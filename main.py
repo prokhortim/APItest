84955511a4be2b17a4ab8da01fc62b6302c4500c
Timofey <tsprokhorov@outlook.com>
2024-10-22 17:14:53 +0300

    add database connection + new api(last but not least!)

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
-
+import loguru
 import datetime
+import psycopg2
+from psycopg2.extras import RealDictCursor
 app1 = FastAPI()
-
+from pydantic import BaseModel
+class validated_user(BaseModel):
+    name:str
+    surname:str
+    age:int
+    registration_date:datetime.date
 @app1.get("/")
 def function():
     return "Hello, world"
@@ -14,5 +21,20 @@ def numbers_sum(a:int,b:int) -> int:
 def date_diff(current_date:datetime.date, offset : int):
     x = (current_date + datetime.timedelta(days = offset))
     return x
+@app1.post("/user/validate", summary = "регистрация(валидация) юзера")
+def validate(user:validated_user):
+    return user.name +" "+ user.surname +" " + str(user.age)+" Years old, registration_date: "+datetime.datetime.strftime(user.registration_date, "%d.%m.%Y:")
+@app1.get("/user/information", summary = "Вывод информации о пользователе")
+def get_information(user_id:int):
+    connection = psycopg2.connect(database = "ba", host = "localhost", user = "postgres", password = "XomiakiNePlachut1", cursor_factory = RealDictCursor)
+    cursor = connection.cursor()
+    cursor.execute(f"""
+    select * from public.mydata
+    where client_id = {user_id}
+    """)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
 
 
